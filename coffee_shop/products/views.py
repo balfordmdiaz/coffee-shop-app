a385cfd0ab77ea36542829d89a266066231d488f
balfordmdiaz <balfordm21@gmail.com>
2024-09-12 21:48:33 -0600

    styles with tailwind css

--- coffee_shop/products/views.py
+++ coffee_shop/products/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import ProductForm
+from .models import Product
 
 class ProductFormView(generic.FormView):
     
@@ -12,4 +13,7 @@ class ProductFormView(generic.FormView):
         form.save()
         return super().form_valid(form)
     
-    
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list_product.html'
+    context_object_name = 'products'
