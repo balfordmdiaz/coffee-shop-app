8baf7b193333b18ee2cd4f88a236f3e9135351c3
balfordmdiaz <balfordm21@gmail.com>
2024-09-20 10:38:53 -0600

    unit test

--- coffee_shop/coffee_shop/settings.py
+++ coffee_shop/coffee_shop/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # For serving static files in production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
